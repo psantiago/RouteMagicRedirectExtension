662b83d3f20cf41792fc9ac76f0c5729e43dbc78
psantiago <Peter.J.Santiago@gmail.com>
2014-05-23 14:16:00 -0400

    initial commit, very fancy!

---
+++ RedirectRouteExtended.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using RouteMagic;
+using RouteMagic.HttpHandlers;
+
+namespace RouteMagicRedirectExtension
+{
+    /// <summary>
+    /// Slightly extended RouteMagic.Internals.RedirectRoute - allows specifying an action to call during redirection.
+    /// Additionally redirects will also keep the query string.
+    /// </summary>
+    public class RedirectRouteExtended : RouteBase, IRouteHandler
+    {
+        public RedirectRouteExtended(RouteBase sourceRoute, RouteBase targetRoute, bool permanent)
+            : this(sourceRoute, targetRoute, permanent, null)
+        {
+        }
+
+        public RedirectRouteExtended(
+            RouteBase sourceRoute,
+            RouteBase targetRoute,
+            bool permanent,
+            RouteValueDictionary additionalRouteValues,
+            Action<RequestContext, RedirectRouteExtended> onRedirectAction = null)
+        {
+            SourceRoute = sourceRoute;
+            TargetRoute = targetRoute;
+            Permanent = permanent;
+            AdditionalRouteValues = additionalRouteValues;
+            OnRedirectAction = onRedirectAction;
+        }
+
+        public Action<RequestContext, RedirectRouteExtended> OnRedirectAction { get; set; }
+
+        public RouteBase SourceRoute
+        {
+            get;
+            set;
+        }
+
+        public RouteBase TargetRoute
+        {
+            get;
+            set;
+        }
+
+        public bool Permanent
+        {
+            get;
+            set;
+        }
+
+        public RouteValueDictionary AdditionalRouteValues
+        {
+            get;
+            private set;
+        }
+
+        public override RouteData GetRouteData(HttpContextBase httpContext)
+        {
+            // Use the original route to match
+            var routeData = SourceRoute.GetRouteData(httpContext);
+            if (routeData == null)
+            {
+                return null;
+            }
+            // But swap its route handler with our own
+            routeData.RouteHandler = this;
+            return routeData;
+        }
+
+        public override VirtualPathData GetVirtualPath(RequestContext requestContext, RouteValueDictionary values)
+        {
+            // Redirect routes never generate an URL.
+            return null;
+        }
+
+        public RedirectRouteExtended To(RouteBase targetRoute)
+        {
+            return To(targetRoute, null);
+        }
+
+        public RedirectRouteExtended To(RouteBase targetRoute, object routeValues)
+        {
+            return To(targetRoute, new RouteValueDictionary(routeValues));
+        }
+
+        public RedirectRouteExtended To(RouteBase targetRoute, RouteValueDictionary routeValues)
+        {
+            if (targetRoute == null)
+            {
+                throw new ArgumentNullException("targetRoute");
+            }
+
+            // Set once only
+            if (TargetRoute != null)
+            {
+                throw new InvalidOperationException(/* TODO */);
+            }
+            TargetRoute = targetRoute;
+
+            // Set once only
+            if (AdditionalRouteValues != null)
+            {
+                throw new InvalidOperationException(/* TODO */);
+            }
+            AdditionalRouteValues = routeValues;
+            return this;
+        }
+
+        public new IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            var requestRouteValues = requestContext.RouteData.Values;
+
+            var routeValues = AdditionalRouteValues.Merge(requestRouteValues);
+
+            var vpd = TargetRoute.GetVirtualPath(requestContext, routeValues);
+
+            //run the onredirection action. For example, this can be used to log that the redirection ocurred.
+            if (OnRedirectAction != null) OnRedirectAction(requestContext, this);
+
+            if (vpd != null)
+            {
+                string targetUrl = "~/" + vpd.VirtualPath;
+
+                //add query strings
+                var qsHelper = requestContext.HttpContext.Request.QueryString;
+                var queryString = String.Join("&", qsHelper.AllKeys.Select(i => i + "=" + qsHelper[i]));
+                if (!string.IsNullOrWhiteSpace(queryString))
+                {
+                    targetUrl += "?" + queryString;
+                }
+
+                return new RedirectHttpHandler(targetUrl, Permanent, isReusable: false);
+            }
+            return new DelegateHttpHandler(rc => rc.HttpContext.Response.StatusCode = 404, requestContext.RouteData, false);
+        }
+    }
+}
---
+++ RouteCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web.Routing;
+using RouteMagic.Internals;
+
+namespace RouteMagicRedirectExtension
+{
+    /// <summary>
+    /// Extends RouteCollection to have a redirect function to add a RedirectRouteExtended instance the RouteCollection.
+    /// </summary>
+    public static class RouteCollectionExtensions
+    {
+        
+        /// <summary>
+        /// We always want to map the RedirectRoute *BEFORE* the legacy route that we're going to redirect.
+        /// Otherwise the redirect route will never match because the legacy route will supersede it. 
+        /// Hence the Func&lt;RouteCollection, RouteBase&gt;.
+        /// </summary>
+        /// <param name="routes"></param>
+        /// <param name="routeMapping"></param>
+        /// <param name="permanent"></param>
+        /// <param name="onRedirectAction">This action will be triggered whenever this redirect happens. This can be useful for logging.</param>
+        /// <returns></returns>
+        public static RedirectRouteExtended Redirect(
+            this RouteCollection routes,
+            Func<RouteCollection, RouteBase> routeMapping,
+            bool permanent = false,
+            Action<RequestContext, RedirectRouteExtended> onRedirectAction = null)
+        {
+            if (routes == null)
+            {
+                throw new ArgumentNullException("routes");
+            }
+            if (routeMapping == null)
+            {
+                throw new ArgumentNullException("routeMapping");
+            }
+
+            var routeCollection = new RouteCollection();
+            var legacyRoute = routeMapping(routeCollection);
+
+            var redirectRoute = new RedirectRouteExtended(legacyRoute, null, permanent, null, onRedirectAction);
+            routes.Add(new NormalizeRoute(redirectRoute));
+            return redirectRoute;
+        }
+    }
+}
